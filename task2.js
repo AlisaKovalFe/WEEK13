98cfae7a8dfe68cc7d06d521544151774a50767e
Alisa <alisakovalhome@mail.ru>
2022-11-01 19:22:26 +0200

    Add task2

---
+++ task2.js
@@ -0,0 +1,27 @@
+function add() 
+{
+    let a = prompt('Ввведите первое число', 'Первое число');
+    let b = prompt('Ввведите второе число', 'Второе число');
+    alert(+a + +b);
+}
+
+function subtract()
+{
+    let a = prompt('Ввведите первое число', 'Первое число');
+    let b = prompt('Ввведите второе число', 'Второе число');
+    alert(a - b)
+}
+
+function multiple()
+{
+    let a = prompt('Ввведите первое число', 'Первое число');
+    let b = prompt('Ввведите второе число', 'Второе число');
+    alert(a * b)
+}
+
+function division()
+{
+    let a = prompt('Ввведите первое число', 'Первое число');
+    let b = prompt('Ввведите второе число', 'Второе число');
+    alert(a / b)
+}
