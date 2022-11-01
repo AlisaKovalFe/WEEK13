1db69b9be9f60c915006088264ef246b6ae72f94
Alisa <alisakovalhome@mail.ru>
2022-11-01 17:02:21 +0200

    Add test answers

---
+++ test.js
@@ -0,0 +1,53 @@
+// 2 вопрос теста
+
+//let name = 'Ilia'
+//alert(`hello ${1}`)
+//alert(`hello ${'name'}`)
+//alert(`hello ${name}`)
+
+// 3 вопрос теста
+
+// var x = 5;
+// var y = 2;
+// var z = x + y;
+// alert(z)
+
+// 6 вопрос теста
+
+// function showX(x) 
+// {
+//     return x;
+// }
+
+// alert(showX(28))
+
+// 7 вопрос теста
+
+x = '\t \n';
+y = 2;
+//z = 5;
+
+alert(x - y)
+
+//8 вопрос теста
+
+// let a = prompt('Первое число?', 1);
+// let b = prompt('Второе число?', 2);
+
+// alert(+a + +b);
+// alert(Number(a) + (Number(b));
+
+// 9 вопрос теста
+
+// x = 5;
+// ++x;
+// x -= 2;
+// alert(x)
+
+// 9 вопрос теста
+
+// function sum(x, y=0)
+// {
+//     return x + y;
+// }
+// alert(sum(5))
