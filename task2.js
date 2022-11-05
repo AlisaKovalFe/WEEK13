97d343aced201d4f32d177dd97a5cb2955823fbd
Alisa <alisakovalhome@mail.ru>
2022-11-05 22:17:38 +0200

    Add if in task2

--- task2.js
+++ task2.js
@@ -2,26 +2,42 @@ function add()
 {
     let a = prompt('Ввведите первое число', 'Первое число');
     let b = prompt('Ввведите второе число', 'Второе число');
-    alert(+a + +b);
+    if (Number(a) && Number(b)) {
+        alert(+a + +b);
+    } else {
+        alert('Не посчитаю, введи числа')
+    }
 }
 
 function subtract()
 {
     let a = prompt('Ввведите первое число', 'Первое число');
     let b = prompt('Ввведите второе число', 'Второе число');
-    alert(a - b)
+    if (Number(a) && Number(b)) {
+        alert(a - b);
+    } else {
+        alert('Не посчитаю, введи числа')
+    }
 }
 
 function multiple()
 {
     let a = prompt('Ввведите первое число', 'Первое число');
     let b = prompt('Ввведите второе число', 'Второе число');
-    alert(a * b)
+    if (Number(a) && Number(b)) {
+        alert(a * b);
+    } else {
+        alert('Не посчитаю, введи числа')
+    }
 }
 
 function division()
 {
     let a = prompt('Ввведите первое число', 'Первое число');
     let b = prompt('Ввведите второе число', 'Второе число');
-    alert(a / b)
+    if (Number(a) && Number(b)) {
+        alert(a / b);
+    } else {
+        alert('Не посчитаю, введи числа')
+    }
 }
